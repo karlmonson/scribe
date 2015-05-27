b05c4d2241341f7aff56115c082de53828d6c350
Karl Monson <karl@idmarketing.co>
2015-05-26 23:37:45 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+Scribe
+=================
+
+The Unofficial PHP Sendy API Wrapper ([http://sendy.co](http://sendy.co))
+
+### Installation
+
+* Place Scribe.php into your file structure
+* Include or require Scribe in the location you would like to utilize it
+
+```php
+  require('lib/Scribe.php');
+```
+
+#Usage
+
+Create an instance of the class while passing in an array including your API key, installation URL, and the List ID you wish to work with.
+```php
+
+  $config = array(
+      'instance' => 'http://example.com/sendy',  //Your Sendy installation
+      'api_key' => 'your_api_key', //your API key is available in Settings
+      'list_id' => 'your_list_id'
+  );
+
+  $scribe = new Scribe($config);
+
+  //you can change the list_id you are referring to at any point
+  $scribe->set_list_id('new_list_id');
+```
+
+#Methods
+After creating a new instance of Scribe call any of the methods below
+
+##Return Values
+The return value of any of these functions will include both a status, and a message to go with that status.
+
+The status is a boolean value of `true` or `false` and the message will vary based on the type of action being performed.
+
+```php
+  //example of a succesful return value
+  array(
+    'status' => 'success',
+    'message' => 'jdoe@example.com subscribed'
+  )
+
+  //example of an unsuccesful return value
+  array(
+    'status' => 'error',
+    'message' => 'jdoe@example.com already subscribed'
+  )
+```
+
+##subscribe(array $values)
+
+This method takes an array of `$values` and will attempt to add the `$values` into the list specified in `$list_id`
+
+```php
+  $results = $scribe->subscribe(array(
+            'name' => 'John Doe',
+            'email' => 'jdoe@example.com', //this is the only field required by sendy
+            'birthday' => '01/01/1990'
+            ));
+```
+__PS:__ Be sure to add any custom fields to the list in Sendy before utilizing them inside this library.
+__PPS:__ If a user is already subscribed to the list, the library will return a status of `true`.
+
+##unsubscribe($email)
+
+Unsubscribes the provided e-mail address (if it exists) from the current list.
+```php
+  $results = $scribe->unsubscribe('jdoe@example.com');
+```
+
+##subscription_status($email)
+
+Returns the status of the user with the provided e-mail address (if it exists) in the current list.
+```php
+  $results = $scribe->subscription_status('jdoe@example.com');
+```
+__PS:__ refer to http://sendy.co/api for the types of return messages you can expect.
+
+##active_subscriber_count()
+
+Returns the number of subscribers to the current list.
+```php
+  $results = $scribe->active_subscriber_count();
+```
+
+##set_list_id($list_id) and get_list_id()
+
+Change or get the list you are currently working with.
+```php
+
+  //set or switch the list id
+  $scribe->set_list_id('new_list_id');
+
+  //get the current list id
+  echo $scribe->get_list_id();
+```
---
+++ lib/Scribe.php
@@ -0,0 +1,215 @@
+<?php
+
+/**
+ * Scribe - The Unofficial Sendy API Wrapper
+ * Copyright © 2015 Karl Monson <karl@karlmonson.com>
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+class Scribe {
+
+    protected $instance;
+    protected $api_key;
+    protected $list_id;
+
+    public function __construct(array $config)
+    {
+        //error checking
+        $instance = @$config['instance'];
+        $api_key = @$config['api_key'];
+        $list_id = @$config['list_id'];
+
+        if (empty($instance)):
+            throw new \Exception("Required config parameter [instance] is not set or empty", 1);
+        elseif (empty($api_key)):
+            throw new \Exception("Required config parameter [api_key] is not set or empty", 1);
+        elseif (empty($list_id)):
+            throw new \Exception("Required config parameter [list_id] is not set or empty", 1);
+        endif;
+
+        $this->instance = $instance;
+        $this->api_key = $api_key;
+        $this->list_id = $list_id;
+    }
+
+    public function set_list_id($list_id)
+    {
+        if (empty($list_id)):
+            throw new \Exception("Required parameter [list_id] is not set", 1);
+        endif;
+
+        $this->list_id = $list_id;
+    }
+
+    public function get_list_id()
+    {
+        return $this->list_id;
+    }
+
+    public function subscribe(array $values)
+    {
+        if (empty($values)):
+            throw new \Exception("Required parameter [values] is not set", 1);
+        endif;
+
+        $return_options = array(
+            'list' => $this->list_id,
+            'boolean' => 'true'
+        );
+        $content = array_merge($values, $return_options);
+        $postdata = http_build_query($content);
+        $opts = array(
+            'http' => array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => $postdata
+            )
+        );
+        $context  = stream_context_create($opts);
+        $result = file_get_contents($this->instance.'/subscribe', false, $context);
+
+        if($result == 1):
+            return array('status' => 'success', 'message' => $email . ' subscribed');
+        else:
+            return array('status' => 'error', 'message' => $result);
+        endif;
+    }
+
+    public function unsubscribe($email)
+    {
+        if (empty($email)):
+            throw new \Exception("Required parameter [email] is not set", 1);
+        endif;
+
+        $postdata = http_build_query(
+            array(
+                'email' => $email,
+                'list' => $this->list_id,
+                'boolean' => 'true'
+            )
+        );
+        $opts = array(
+            'http' => array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => $postdata
+            )
+        );
+        $context  = stream_context_create($opts);
+        $result = file_get_contents($this->instance.'/unsubscribe', false, $context);
+
+        if($result == 1):
+            return array('status' => 'success', 'message' => $email . ' unsubscribed');
+        else:
+            return array('status' => 'error', 'message' => $result);
+        endif;
+    }
+
+    public static function subscription_status($email)
+    {
+        if (empty($email)):
+            throw new \Exception("Required parameter [email] is not set", 1);
+        endif;
+
+        $postdata = http_build_query(
+            array(
+                'api_key' => $this->api_key,
+                'email' => $email,
+                'list_id' => $this->list_id
+            )
+        );
+        $opts = array(
+            'http' => array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => $postdata
+            )
+        );
+        $context  = stream_context_create($opts);
+        $result = file_get_contents($this->instance.'//api/subscribers/subscription-status.php', false, $context);
+
+        if($result == 'Subscribed' || $result == 'Unsubscribed' || $result == 'Unconfirmed' || $result == 'Bounced' || $result == 'Soft bounced' || $result == 'Complained'):
+            return array('status' => 'success', 'message' => $result);
+        else:
+            return array('status' => 'error', 'message' => $result);
+        endif;
+    }
+
+    public static function active_subscriber_count()
+    {
+        $postdata = http_build_query(
+            array(
+                'api_key' => $this->api_key,
+                'list_id' => $this->list_id
+            )
+        );
+        $opts = array(
+            'http' => array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => $postdata
+            )
+        );
+        $context  = stream_context_create($opts);
+        $result = file_get_contents($this->instance.'//api/subscribers/active-subscriber-count.php', false, $context);
+
+        if(is_numeric($result)):
+            return array('status' => 'success', 'message' => $result);
+        else:
+            return array('status' => 'error', 'message' => $result);
+        endif;
+    }
+
+    public static function create_campaign($from_name, $from_email, $reply_to, $subject, $plain_text, $html_text, $list_ids = null, $brand_id = null, $send_campaign = null)
+    {
+        $postdata = http_build_query(
+            array(
+                'api_key' => $this->api_key,
+                'from_name' => $from_name,
+                'from_email' => $from_email,
+                'reply_to' => $reply_to,
+                'subject' => $subject,
+                'plain_text' => $plain_text,
+                'html_text' => $html_text,
+                'list_ids' => ($list_ids ? $list_ids : $this->list_id),
+                'brand_id' => ($brand_id ? $brand_id : ''),
+                'send_campaign' => ($send_campaign ? $send_campaign : 0)
+            )
+        );
+        $opts = array(
+            'http' => array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => $postdata
+            )
+        );
+        $context  = stream_context_create($opts);
+        $result = file_get_contents($this->instance.'//api/campaigns/create.php', false, $context);
+
+        if($result == 'Campaign created' || $result == 'Campaign created and now sending'):
+            return array('status' => 'success', 'message' => $result);
+        else:
+            return array('status' => 'error', 'message' => $result);
+        endif;
+    }
+
+}
