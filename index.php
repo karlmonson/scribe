5490c94cf07496ff74d1364f9f76f799a6da6097
Karl Monson <karl@idmarketing.co>
2015-05-26 23:41:53 -0600

    updated index.php

--- index.php
+++ index.php
@@ -2,20 +2,10 @@
 
     require('lib/Scribe.php');
 
-    // $config = array(
-    //     'instance' => 'http://example.com/sendy',  //Your Sendy installation
-    //     'api_key' => 'your_api_key', //your API key is available in Settings
-    //     'list_id' => 'your_list_id'
-    // );
-
     $config = array(
-        'instance' => 'http://tryecigstoday.com/sendy',  //Your Sendy installation
-        'api_key' => 'a8yRF9ghfJLwMrXvQp1u', //your API key is available in Settings
-        'list_id' => 'vKTLgs2b8920pNglyRccnpFw'
+        'instance' => 'http://example.com/sendy',  //Your Sendy installation
+        'api_key' => 'your_api_key', //your API key is available in Settings
+        'list_id' => 'your_list_id'
     );
 
     $scribe = new Scribe($config);
-
-    $result = $scribe->subscribe(array('email' => 'karlos@santana.com', 'Birthday' => '11/10/1990'));
-
-    echo $result['status'] . ': ' . $result['message'];
